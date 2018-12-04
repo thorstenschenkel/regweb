1370c86c57c22311fb7d283766485c02012840d4
thorstenschenkel <thorsten.schenkel@compeople.de>
2018-12-04 17:41:26 +0100

    selector for event added

--- app/src/components/EventPicker.vue
+++ app/src/components/EventPicker.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
-    <h2>Year</h2>
-    <select v-model="selected">
-      <option disabled value>Select the year of the event</option>
-      <option v-for="year in years">{{ year }}</option>
-    </select>
-    {{ selected }}
+    <div>
+      <h2>Year</h2>
+      <select v-model="selectedYear" v-on:change="onYearChanged">
+        <option disabled value>Select a year</option>
+        <option v-for="year in years">{{ year }}</option>
+      </select>
+      <span v-if="selectedYear">{{ selectedYear }}</span>
+    </div>
+    <div v-if="selectedYear">
+      <h2>Event</h2>
+      <select v-model="selectedEvent">
+        <option disabled value>Select an event</option>
+        <option v-for="event in events" v-bind:value="event.id">{{ event.name }}</option>
+      </select>
+      <span v-if="selectedEvent">{{ selectedEvent }}</span>
+    </div>
   </div>
 </template>
 
 <script>
+import Vue from "vue";
 import { EventBus } from "./../shared/eventBus";
 import { Event } from "./../shared/eventEnum";
 
@@ -17,15 +28,18 @@ export default {
   data() {
     return {
       years: ["2018", "2019"],
-      selected: ""
+      events: [{ id: 1, name: "abc" }, { id: 2, name: "xyz" }],
+      selectedYear: null,
+      selectedEvent: null
     };
   },
   created() {
+    this.that = this;
     EventBus.$emit(Event.LOADING, true);
     // const url =
     //   "https://bl78p0futl.execute-api.eu-west-1.amazonaws.com/public/regapi";
     const url = "http://jsonplaceholder.typicode.com/posts";
-    this.$http
+    Vue.http
       .get(url)
       .then(data => {
         console.log("data: ", data);
@@ -36,6 +50,25 @@ export default {
       .finally(() => {
         EventBus.$emit(Event.LOADING, false);
       });
+  },
+  methods: {
+    onYearChanged: () => {
+      EventBus.$emit(Event.LOADING, true);
+      // const url =
+      //   "https://bl78p0futl.execute-api.eu-west-1.amazonaws.com/public/regapi";
+      const url = "http://jsonplaceholder.typicode.com/posts";
+      Vue.http
+        .get(url)
+        .then(data => {
+          console.log("data: ", data);
+        })
+        .catch(error => {
+          console.error("Cannot load the events!", error);
+        })
+        .finally(() => {
+          EventBus.$emit(Event.LOADING, false);
+        });
+    }
   }
 };
 </script>
